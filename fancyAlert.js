e9b8d30d0819e2c4a65952d06d432e9474d9cefd
Balthazar <BDeliers@users.noreply.github.com>
2017-02-15 14:20:02 +0100

    Update fancyAlert.js

--- fancyAlert.js
+++ fancyAlert.js
@@ -6,10 +6,10 @@
 	Yous should put this function in your <head>, your script file, or at the top of your <body>
 
 	The call has this typical look :  
-	fancyAlert({bgColor:"background color", txtColor:"text color", height:"height", fontSize:"font size", length: duration in seconds, msg="your message"})
+	fancyAlert({bgColor:"background color", txtColor:"text color", height:"height", fontSize:"font size", length: duration in seconds, msg:"your message"})
 	For example :
 	
-	fancyAlert({bgColor:"#FE3123", txtColor:"#222", height:"85px", fontSize:"20px", length: 3, msg="Hi everybody !"})
+	fancyAlert({bgColor:"#FE3123", txtColor:"#222", height:"85px", fontSize:"20px", length: 3, msg:"Hi everybody !"})
 
 	©BDeliers, février 2017
 	http://bdeliers.com	
